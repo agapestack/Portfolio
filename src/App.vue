b5f7ba142b6bc3d74c7faa3cca2d0c86ab0fc09f
agapestack <agapestack@gmail.com>
2021-05-17 01:28:56 +0200

    removing extra code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
   <div id="app" class="app">
-    <div class="sections-menu">
-      <span
-         class="menu-point"
-         v-bind:class="{active: activeSection == index}"
-         v-on:click="scrollToSection(index)"
-         v-for="(offset, index) in offsets"
-         v-bind:key="index">
-      </span>
-    </div>
 
     <Navbar />
   
@@ -17,10 +8,6 @@
       <section><Projects class="views  fullpage container" id="projects"/></section>
       <section><About class="views  fullpage container" id="about"/></section>
       <section><Contact class="views  fullpage container" id="contact"/></section>
-      
-      
-      
-      
     </main>
 
     <section>
@@ -46,111 +33,6 @@ export default {
     Projects,
     Footer
   },
-  data() {
-    return {
-      inMove: false,
-      activeSection: 0,
-      offsets: [],
-      touchStartY: 0,
-    }
-  },
-  methods: {
-    calculateSectionOffsets() {
-      let sections = document.getElementsByTagName('section');
-      let length = sections.length;
-      
-      for(let i = 0; i < length; i++) {
-        let sectionOffset = sections[i].offsetTop;
-        this.offsets.push(sectionOffset);
-      }
-    },
-    handleMouseWheel: function(e) {
-      
-      if (e.wheelDelta < 30 && !this.inMove) {
-        this.moveUp();
-      } else if (e.wheelDelta > 30 && !this.inMove) {
-        this.moveDown();
-      }
-        
-      e.preventDefault();
-      return false;
-    },
-    handleMouseWheelDOM: function(e) {
-      
-      if (e.detail > 0 && !this.inMove) {
-        this.moveUp();
-      } else if (e.detail < 0 && !this.inMove) {
-        this.moveDown();
-      }
-      
-      return false;
-    },
-    moveDown() {
-      this.inMove = true;
-      this.activeSection--;
-        
-      if(this.activeSection < 0) this.activeSection = this.offsets.length - 1;
-        
-      this.scrollToSection(this.activeSection, true);
-    },
-    moveUp() {
-      this.inMove = true;
-      this.activeSection++;
-        
-      if(this.activeSection > this.offsets.length - 1) this.activeSection = 0;
-        
-      this.scrollToSection(this.activeSection, true);
-    },
-    scrollToSection(id, force = false) {
-      if(this.inMove && !force) return false;
-      
-      this.activeSection = id;
-      this.inMove = true;
-      
-      document.getElementsByTagName('section')[id].scrollIntoView({behavior: 'smooth'});
-      
-      setTimeout(() => {
-        this.inMove = false;
-      }, 400);
-      
-    },
-    touchStart(e) {
-      e.preventDefault();
-      
-      this.touchStartY = e.touches[0].clientY;
-    },
-    touchMove(e) {
-      if(this.inMove) return false;
-      e.preventDefault();
-      
-      const currentY = e.touches[0].clientY;
-      
-      if(this.touchStartY < currentY) {
-        this.moveDown();
-      } else {
-        this.moveUp();
-      }
-      
-      this.touchStartY = 0;
-      return false;
-    }
-  },
-  created() {
-    this.calculateSectionOffsets();
-    
-    window.addEventListener('DOMMouseScroll', this.handleMouseWheelDOM);  // Mozilla Firefox
-    window.addEventListener('mousewheel', this.handleMouseWheel, { passive: false }); // Other browsers
-    
-    window.addEventListener('touchstart', this.touchStart, { passive: false }); // mobile devices
-    window.addEventListener('touchmove', this.touchMove, { passive: false }); // mobile devices
-  },
-  destroyed() {
-    window.removeEventListener('mousewheel', this.handleMouseWheel, { passive: false });  // Other browsers
-    window.removeEventListener('DOMMouseScroll', this.handleMouseWheelDOM); // Mozilla Firefox
-    
-    window.removeEventListener('touchstart', this.touchStart); // mobile devices
-    window.removeEventListener('touchmove', this.touchMove); // mobile devices
-  }
 }
 </script>
 
